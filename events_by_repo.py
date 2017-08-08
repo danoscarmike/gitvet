4551d428c779d6041cc4a4acb7e7b1d0c1da704a
danoscarmike <omeara.dan@gmail.com>
2017-08-08 08:17:39 -0700

    events_by_repo

---
+++ events_by_repo.py
@@ -0,0 +1,32 @@
+import csv
+import os
+import pytz
+import six
+import sys
+
+from datetime import datetime as dt
+
+import github3
+
+org = six.text_type(sys.argv[1])
+
+with open(sys.argv[2]) as f:
+    repos = f.readlines()
+repos = [x.strip() for x in repos]
+
+# Authenticate with GitHub using Personal Access Token
+g = github3.login(token=os.environ['GH_TOKEN'])
+
+file_time = dt.now(pytz.utc).strftime("%Y%m%d_%H:%M%Z")
+
+with open('../output_files/%s_all_events.csv' % file_time,
+          'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for repo in repos:
+        print('Getting events from %s' % repo)
+        for event in g.repository(org, repo).events(10):
+            if event.type in ['IssuesEvent', 'IssueCommentEvent', 'LabelEvent']:
+                row = [repo, event.id,
+                       event.type, event.as_dict()['payload']['action'],
+                       event.created_at, event.actor]
+                writer.writerow(row)
