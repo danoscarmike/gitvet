f80fdc5b703aa1d0dd0f6908672c973359636b6a
danoscarmike <omeara.dan@gmail.com>
2018-01-08 15:57:16 -0800

    introduce etag handling

--- get_closed_issue_count.py
+++ get_closed_issue_count.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 
+import collections
 import csv
 import json
 import os
@@ -21,33 +22,14 @@ def main(gh_login, repo_file):
 
     """
     Function uses the GitHub v3 API to read the issues for each repo
-    in the list provided.  The following issue metadata is extracted
-    and returned in a JSON-like object.
+    in the list provided.
 
     Args:
         gh_login: an authenticated GitHub session.
-        repos: list of GitHub repository names.
-        state: state of the issues to get via the API - can be 'open',
-               'closed' or 'all'.
+        repo_file: file containing list of GitHub repository names.
 
     Returns:
-        A JSON-like file of issue metadata:
-        'repoorg/reponame': {
-            'open_issues_count': <int>,
-            'prs': {'open_pr_count': <int>,
-                    'pr_aggregate_age': <int>},
-            'issues': {
-                issue.number: {
-                    assignee: <string>,
-                    created: <isoformat date>,
-                    labels: [
-                        <string>, ...
-                    ]
-                    title: <string>,
-                    updated: <isoformat data>
-                }
-            }
-        }
+        Writes to CSV file
     """
     #set pytz.timezone variable
     pacific = pytz.timezone('US/Pacific')
@@ -58,35 +40,61 @@ def main(gh_login, repo_file):
     repos = [x.strip() for x in repos]
     f.close()
 
-    with open('../output_files/closed_issue_counts.csv', 'a') as csvfile:
-        fieldnames = ['repo', 'issue.number', 'closed_at']
-        datawriter = csv.DictWriter(csvfile, delimiter=',',
-                                    fieldnames=fieldnames, quotechar='"')
-        for repo in repos:
-            # get issue generator from GitHub
-            print('Getting issues from %s' % repo)
-            org, repo_name = repo.split("/")
-            issues = gh_login.issues_on(org, repo_name, state='closed')
-            for issue in issues:
-                # check remaining rate limit
-                if crl.remaining() > 100:
-                    pass
-                else:
-                    print('Rate limit low; going to sleep until rate resets')
-                    crl.print_remaining()
-                    time.sleep(crl.reset() - int(time.time()))
-                if issue.pull_request():
-                    continue
-                else:
-                    # add this issue to the issues sub-dictionary in the
-                    # data dictionary (key is GitHub issue number)
-                    closed_date = issue.closed_at.astimezone(pacific)
-                    closed_date = closed_date.strftime('%m/%d/%Y')
-                    row = {'repo': repo, 'issue.number': issue.number,
-                           'closed_at': closed_date}
-                    datawriter.writerow(row)
-                    print('%d written to file' % issue.number)
-    csvfile.close()
+    with open('closed_etag_file.json', 'r+') as etag_file:
+        try:
+            etags = json.load(etag_file)
+        except:
+            print("No JSON data in 'closed_etag_file.json'")
+            etags = {}
+
+        with open('../output_files/closed_issue_counts.csv', 'a') as csvfile:
+            fieldnames = ['repo', 'issue.number', 'closed_at']
+            datawriter = csv.DictWriter(csvfile, delimiter=',',
+                                        fieldnames=fieldnames, quotechar='"')
+            for repo in repos:
+                # get issue generator from GitHub
+                print('Getting issues from %s' % repo)
+                org, repo_name = repo.split("/")
+                try:
+                    etag = etags[org][repo_name]
+                    print "Etag found. Passing to API call."
+                    issues = gh_login.issues_on(org, repo_name, state='closed',
+                                                etag=etag)
+                except:
+                    print("No etag found for %s/%s." % (org, repo_name))
+                    issues = gh_login.issues_on(org, repo_name, state='closed')
+
+                for issue in issues:
+                    # check remaining rate limit
+                    if crl.remaining() > 100:
+                        pass
+                    else:
+                        print('Rate limit low; going to sleep until rate resets')
+                        crl.print_remaining()
+                        time.sleep(crl.reset() - int(time.time()))
+                    if issue.pull_request():
+                        continue
+                    else:
+                        # add this issue to the issues sub-dictionary in the
+                        # data dictionary (key is GitHub issue number)
+                        closed_date = issue.closed_at.astimezone(pacific)
+                        closed_date = closed_date.strftime('%m/%d/%Y')
+                        row = {'repo': repo, 'issue.number': issue.number,
+                               'closed_at': closed_date}
+                        datawriter.writerow(row)
+                        print('%d written to file' % issue.number)
+                try:
+                    etags[org][repo_name] = issues.etag
+                except:
+                    etags[org] = {}
+                    etags[org][repo_name] = issues.etag
+                print etags
+            csvfile.close()
+        etag_file.seek(0)
+        json.dump(etags, etag_file)
+        etag_file.truncate()
+    etag_file.close()
+
 
 if __name__ == "__main__":
     g = github3.login(token=os.environ['GH_TOKEN'])
