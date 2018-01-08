9b7651843eebc350a18e3dca6876096c5024b9c2
danoscarmike <omeara.dan@gmail.com>
2018-01-08 13:47:43 -0800

    remove redundant dict/JSON

--- get_closed_issue_count.py
+++ get_closed_issue_count.py
@@ -49,68 +49,44 @@ def main(gh_login, repo_file):
             }
         }
     """
+    #set pytz.timezone variable
+    pacific = pytz.timezone('US/Pacific')
 
     #extract list of repos from text file
     with open(repo_file) as f:
         repos = f.readlines()
     repos = [x.strip() for x in repos]
+    f.close()
 
-    pacific = pytz.timezone('US/Pacific')
-
-    # Initialize dictionary for results
-    data = {}
-    for repo in repos:
-        # set up repo specific variables, data structures and counters
-        data[repo] = {}
-
-        # get issue generator from GitHub
-        print('Getting issues from %s' % repo)
-        org, repo_name = repo.split("/")
-        issues = gh_login.issues_on(org, repo_name, state='closed')
-
-        for issue in issues:
-            # check remaining rate limit
-            if crl.remaining() > 100:
-                pass
-            else:
-                print('Rate limit low; going to sleep until rate resets')
-                crl.print_remaining()
-                time.sleep(crl.reset() - int(time.time()))
-
-            if issue.pull_request():
-                continue
-            else:
-                print('Adding issue number: %d' % issue.number)
-                # add this issue to the issues sub-dictionary in the
-                # data dictionary (key is GitHub issue number)
-                closed_date = issue.closed_at.astimezone(pacific)
-                data[repo][issue.number] = closed_date.strftime('%m/%d/%Y')
-
-    data['updated'] = dt.now(pacific).isoformat()
-
-    # print json.dumps(data, indent = 4, sort_keys=True)
-
-    file_time = dt.now(pacific).strftime("%Y%m%d_%H%M%Z")
-
-    with open('../output_files/%s_closed_issue_counts.csv' % file_time,
-              'w') as csvfile:
+    with open('../output_files/closed_issue_counts.csv', 'a') as csvfile:
         fieldnames = ['repo', 'issue.number', 'closed_at']
-
         datawriter = csv.DictWriter(csvfile, delimiter=',',
                                     fieldnames=fieldnames, quotechar='"')
-        datawriter.writeheader()
-        for repo, repo_data in data.items():
-            if repo == 'updated':
-                continue
-            else:
-                row = {'repo': repo}
-                # print(repo_data)
-                # print type(repo_data)
-                for issue, issue_data in repo_data.items():
-                    row.update({'issue.number': issue,
-                                'closed_at': data[repo][issue]})
+        for repo in repos:
+            # get issue generator from GitHub
+            print('Getting issues from %s' % repo)
+            org, repo_name = repo.split("/")
+            issues = gh_login.issues_on(org, repo_name, state='closed')
+            for issue in issues:
+                # check remaining rate limit
+                if crl.remaining() > 100:
+                    pass
+                else:
+                    print('Rate limit low; going to sleep until rate resets')
+                    crl.print_remaining()
+                    time.sleep(crl.reset() - int(time.time()))
+                if issue.pull_request():
+                    continue
+                else:
+                    # add this issue to the issues sub-dictionary in the
+                    # data dictionary (key is GitHub issue number)
+                    closed_date = issue.closed_at.astimezone(pacific)
+                    closed_date = closed_date.strftime('%m/%d/%Y')
+                    row = {'repo': repo, 'issue.number': issue.number,
+                           'closed_at': closed_date}
                     datawriter.writerow(row)
-        print('Written to file.')
+                    print('%d written to file' % issue.number)
+    csvfile.close()
 
 if __name__ == "__main__":
     g = github3.login(token=os.environ['GH_TOKEN'])
