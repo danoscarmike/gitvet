1baf10ee82745cba92f6cc7e9987f3c8b2b9a79e
danoscarmike <omeara.dan@gmail.com>
2018-07-27 15:38:20 -0700

    more scaffolding

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='gitvet',
       version='0.1',
       description='Tools for GitHub issue triage',
+      long_description='readme()'
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -14,6 +21,7 @@ setup(name='gitvet',
       license='MIT',
       packages=['gitvet'],
       install_requires=[
+          'click',
           'github3.py', 
           'pytz'
       ],
